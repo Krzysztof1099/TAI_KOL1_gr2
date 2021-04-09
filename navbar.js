b8f091444e3d9eae2edc4e0e3149ec0dee2184df
Krzysztof Piechnik <krzysztofpiechnik1999@interia.pl>
2021-04-09 15:31:31 +0200

    Wykonane zadanie 3

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let Nawigacja = '<nav><a href="index.html">Home</a>' + ' ' + '<a href="kontakt.html">Kontakt</a><nav>';
+
+let navbarElem = document.querySelector(".nav-bar")
+
+navbarElem.innerHTML = Nawigacja;
